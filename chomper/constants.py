79f1e896517bd9d16d0a66ec1e7048530db7758e
Aniket Panjwani <aniketpanjwani2018@u.northwestern.edu>
2018-02-16 10:10:50 -0600

    Remove TODO.

--- chomper/constants.py
+++ chomper/constants.py
@@ -2,5 +2,4 @@
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 TIME_FORMAT = "%Y-%m-%d %H:%M:%S"
-#TODO: mitmdump should be a direct path since I'm calling it as sudo.
 MITMDUMP_COMMAND = 'screen -d -m {} --set allow_remote=true --mode transparent --showhost -s ./chomper/filter.py --set "addresses_str={}" --set "rule_type={}"'
